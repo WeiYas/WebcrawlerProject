3ef7fff8bbee6801e9100e67c3ebd5312f5c333e
WeiYas <yasmine.weiwert@gmail.com>
2024-04-29 21:47:10 +0200

    trying mongoconnection

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
 import streamlit as st
+import pymongo
 import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+#App Title
+st.title('your first mongoDB data visualization😎')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+@st.cache_resource
+def init_connection():
+    connection_string = st.secrets["mongo"]["connection_string"]
+    return pymongo.MongoClient(connection_string)
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+client = init_connection()
+db = client.universities_rank
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+@st.cache_data(ttl=600)
+def get_universities():
+    collection = db['universities'].find({})
+    return pd.DataFrame(collection)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+#disply the dataframe
+universities = get_universities()
+st.write(universities)
